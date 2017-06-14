46c840ec431d6d3a3d8d75eefdc0f5c4a697ee8d
bradendavis <bradendavis@192.168.0.3>
2017-06-13 21:37:47 -0400

    updated main

--- src/net/minewave/Main.java
+++ src/net/minewave/Main.java
@@ -1,1 +1,16 @@
+package net.minewave;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+import me.Turttles.TRegister;
+
+public class Main extends JavaPlugin {
+	
+	
+	
+	@Override
+	public void onEnable() {
+		
+		new TRegister(this);
+	}
+}
