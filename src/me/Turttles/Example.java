c07984e6100211dee38742ceaf01cf6bc6585cc4
bradendavis <bradendavis@192.168.0.3>
2017-06-13 21:02:24 -0400

    added playerJoin event

--- src/me/Turttles/Example.java
+++ src/me/Turttles/Example.java
@@ -1,1 +1,24 @@
+package me.Turttles;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class Example implements Listener {
+	
+	
+	
+	@EventHandler
+	public void playerJoin(PlayerJoinEvent e) {
+		
+		Player p = e.getPlayer();
+		
+		e.setJoinMessage(p.getName() + " did a join.");
+		
+		p.sendMessage("you did a join");
+		
+		
+	}
+	
+}
